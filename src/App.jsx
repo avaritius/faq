e262bdf5abe9bdf4ad48e133696d3f3bb0987201
John Doe <johndoe@example.com>
2025-10-28 11:59:34 +0100

    dodanie pola wyboru rodzaju filmu do formularza

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [tytul, setTytul] = useState(''); 
+  const [tytul, setTytul] = useState('');
+  const [rodzaj, setRodzaj] = useState('');
 
   return (
     <div className="container mt-5">
@@ -18,6 +19,22 @@ function App() {
             onChange={(e) => setTytul(e.target.value)}
             required
           />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="rodzaj" className="form-label">rodzaj</label>
+          <select
+            className="form-select"
+            id="rodzaj"
+            value={rodzaj}
+            onChange={(e) => setRodzaj(e.target.value)}
+            required
+          >
+            <option value=""></option>
+            <option value="1">komedia</option>
+            <option value="2">obyczajowy</option>
+            <option value="3">sensacyjny</option>
+            <option value="4">horror</option>
+          </select>
         </div>
       </form>
     </div>
