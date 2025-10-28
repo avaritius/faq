62530d9939fb436203037776f54dafef94966144
John Doe <johndoe@example.com>
2025-10-28 11:39:13 +0100

    dodanie bootstrapa bo czm nie (w sensie przyda sie cn) i dodanie takiego podstawowego formularza ktory podobno niby dziala (wlasciwie to dodanie pola textowego i 2 napisow

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tytul, setTytul] = useState(''); 
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container mt-5">
+      <h1>dodaj film</h1>
+      <form>
+        <div className="mb-3">
+          <label htmlFor="tytul" className="form-label">tytul filmu</label>
+          <input
+            type="text"
+            className="form-control"
+            id="tytul"
+            value={tytul}
+            onChange={(e) => setTytul(e.target.value)}
+            required
+          />
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
